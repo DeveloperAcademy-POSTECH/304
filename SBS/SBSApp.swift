9d0613a3e90f0081f207f48bea3d730e3ba07964
Inho Choi <55151796+E-know@users.noreply.github.com>
2022-11-30 01:21:24 +0900

    Initial commit

---
+++ SBS/SBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBSApp.swift
+//  SBS
+//
+//  Created by Inho Choi on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct SBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
